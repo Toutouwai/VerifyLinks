807ed2369b97019d606b7ff56ac98c3d7c28dacb
Robin Sallis <Toutouwai@users.noreply.github.com>
2023-12-09 18:32:14 +1300

    v0.1.2

--- ProcessVerifyLinks.js
+++ ProcessVerifyLinks.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
-	// Note: this file also loads in ProcessModule when configuring ProcessVerifyLinks
 
-	// Initialise DataTable if the target element exists
-	const $vl_datatable = $('#vl-datatable');
-	if($vl_datatable.length) {
-		$vl_datatable.DataTable({
-			dom: '<"top-controls-wrap"Bf>r<"table-wrap"t><"bottom-controls-wrap"lip>',
-			columnDefs: [
-				{
-					targets: [1, 5], // Hide View and Redirect columns by default
-					visible: false,
-				},
-			],
-			order: [
-				[3, 'desc'],
-				[4, 'desc'],
-			], // Sort by Alert column, then by Code column
-			pageLength: ProcessWire.config.ProcessVerifyLinks.tableLimit || 25,
-			buttons: [
-				'colvis',
-			],
-			stripeClasses: [],
-			bSortClasses: false,
-		});
-	}
+	// Return early if this is the module config screen (ProcessModule)
+	if($('body').hasClass('ProcessModule')) return;
+
+	// Initialise DataTable
+	$('#vl-datatable').DataTable({
+		dom: '<"top-controls-wrap"Bf>r<"table-wrap"t><"bottom-controls-wrap"lip>',
+		columnDefs: [
+			{
+				targets: [5], // Hide Redirect column by default
+				visible: false,
+			},
+		],
+		order: [
+			[3, 'desc'],
+			[4, 'desc'],
+		], // Sort by Alert column, then by Code column
+		pageLength: ProcessWire.config.ProcessVerifyLinks.tableLimit || 25,
+		buttons: [
+			'colvis',
+		],
+		stripeClasses: [],
+		bSortClasses: false,
+	});
 
 });
--- VerifyLinks.info.php
+++ VerifyLinks.info.php
@@ -3,11 +3,11 @@
 $info = array(
 	'title' => 'Verify Links',
 	'summary' => 'Periodically verifies that external links are working and not leading to an error page.',
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'author' => 'Robin Sallis',
 	'href' => 'https://github.com/Toutouwai/VerifyLinks',
 	'icon' => 'link',
 	'autoload' => true,
-	'requires' => 'ProcessWire>=3.0.216, PHP>=7.0.0, LazyCron',
+	'requires' => 'ProcessWire>=3.0.0, PHP>=7.0.0, LazyCron',
 	'installs' => 'ProcessVerifyLinks',
 );
